732ceef75591ac1a0e56e9b0485c7563c36c7781
gtxiqbal <moh.iqbal.alhusain@gmail.com>
2019-12-23 10:07:38 +0700

    Beta

--- app_indihome/settings.py
+++ app_indihome/settings.py
@@ -128,5 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "/static/"
 
-STATIC_URL = '/static/'
+django_heroku.settings(locals())
